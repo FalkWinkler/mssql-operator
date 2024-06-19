0472c97e4ef11bae19f0c8e5ed4e04c98648711f
Falk Winkler <fwinkler0573@gmail.com>
2024-06-19 02:42:00 +0200

    variablen hin zugefügt

---
+++ variablesdatabasecluster/variablesdatabasecluster.go
@@ -0,0 +1,52 @@
+package variablesdatabasecluster
+
+import (
+	"fmt"
+)
+
+var StatefulSetName string
+var ServiceName string
+var ImageName string
+var ClusterRoleName string
+var ClusterRoleBindingName string
+var ContainerName string
+var DataDirectory string = "/data"
+var Port int32
+var StorageResourceRequirement = "1Gi"
+
+// To improve portability between OpenShift environments, we rely on the cluster providing a default storage class
+//var StorageClassName string = "ibmc-vpc-block-5iops-tier"
+
+const ANNOTATION_TITLE = "applications.application.sample.ibm.com/title"
+const DEFAULT_ANNOTATION_TITLE = "My Title"
+const LabelKey = "app"
+const LabelValue = "database-cluster"
+const VolumeMountName = "data-volume"
+
+const EnvKeyDataDirectory = "DATA_DIRECTORY"
+const EnvKeyPodname = "POD_NAME"
+const EnvKeyNamespace = "NAMESPACE"
+
+const RoleBindingServiceAccount = "default"
+
+func SetGlobalVariables(applicationName string, image string) {
+	if applicationName == "" {
+		applicationName = "database"
+	}
+
+	StatefulSetName = applicationName + "-cluster"
+	ServiceName = applicationName + "-service"
+	ClusterRoleName = applicationName + "-role"
+	ClusterRoleBindingName = applicationName + "-rolebinding"
+	ContainerName = applicationName + "-app"
+	ImageName = image
+	Port = 1433
+}
+
+func PrintVariables(databaseName string, databaseNamespace string, amountPods int32) {
+	fmt.Println("Custom Resource Values:")
+	fmt.Printf("- Name: %s\n", databaseName)
+	fmt.Printf("- Namespace: %s\n", databaseNamespace)
+	fmt.Printf("- AmountPods: %d\n", amountPods)
+	fmt.Printf("- Image: %s\n", ImageName)
+}
